e02a31c1bd3543738dabc4a08eea7164d5e7ac10
naru0u <1912305@mwu.jp>
2025-06-09 20:49:11 +0900

    コミット

---
+++ web/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Spring Bootアプリケーションの起動
+Java -jar /var/www/java/DailyReportSystemApplication-0.0.1-SNAPSHOT.jar &
+
+# Apache2の起動
+rm -f /ver/run/apache2/apache2.pid
+apachectl -D FOREGROUND
